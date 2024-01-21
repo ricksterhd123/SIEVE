8dd44b5345e021cf381a2f94a2a71f3530345306
ricksterhd123 <ricksterhd123@gmail.com>
2024-01-21 18:50:42 +0000

    add shebang and set -e to script files

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-docker buildx build -f Dockerfile --load -t sieve .
+#!/usr/bin/env bash
+
+set -e
+
 mkdir -p build
+docker buildx build -f Dockerfile --load -t sieve .
 docker container run -v "./build:/sieve/build" sieve:latest
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
 bash build.sh > /dev/null 2>&1
 node build/SIEVE.js
