de8e5b0faf8133e2317d5517ac0f3b0c1c3c7820
ricksterhd123 <ricksterhd123@gmail.com>
2024-01-21 17:58:41 +0000

    try run from github actions

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 docker build -f Dockerfile -t sieve .
 mkdir -p build
 docker container run -v "./build:/sieve/build" sieve:latest
-clear
-docker container run -v "$PWD/build":/usr/src/app -w /usr/src/app node:latest node SIEVE.js
